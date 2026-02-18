c2462da0a1c5fade25788cae5d7ec9420d9b2bfd
Sarah Parisi <siparisi@uncg.edu>
2026-02-18 13:21:10 -0500

    added the button and price

---
+++ script/script.js
@@ -0,0 +1,25 @@
+
+const components = document.querySelectorAll(".dish-card");
+
+components.forEach(currentComponent => {
+    // create new section for items
+    const priceSection = document.createElement("div")
+    priceSection.classList.add("price-section")
+
+    // Create Fav button
+    const favButton = document.createElement("button");
+    favButton.textContent = "Favorite";
+
+    // Create new Price label
+    const priceLabel = document.createElement("span");
+    priceLabel.textContent = "$5.00";
+
+    // Append button and label to price section
+    priceSection.appendChild(favButton);
+    priceSection.appendChild(priceLabel);
+
+    // Append price section to dish card
+    currentComponent.appendChild(priceSection);
+});
+
+
